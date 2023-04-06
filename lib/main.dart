3f97699506ad52c006e2febe690598e782b47798
djinodissingar <dissingardjino@icloud.com>
2023-04-06 10:26:11 +0200

    mise en place des fichiers de configurations 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:google_ml_kit/google_ml_kit.dart';
 import 'firebase_options.dart';
 
 void main() async {
@@ -54,27 +55,62 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  TextEditingController controller = TextEditingController();
+  late LanguageIdentifier _languageIdentifier;
+  String simpleLang = "";
+  String mutlipleLang = "";
 
-  void _incrementCounter() {
+
+
+  // méthode
+  getUniqueLanguage() async {
+    if(controller.text == "") return;
+    String phrase = controller.text;
+    String langageIdentified = await _languageIdentifier.identifyLanguage(phrase);
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      simpleLang = langageIdentified;
     });
+
+  }
+
+  getMultipleLanguage() async{
+    String phrase = controller.text;
+    if(phrase == "") return;
+    final multiple = await _languageIdentifier.identifyPossibleLanguages(phrase);
+    if(multiple.isEmpty){
+      setState(() {
+        mutlipleLang = "Nous n'avons pas trouvé aucune correspondance";
+      });
+
+    }
+    else
+      {
+        for(var lang in multiple){
+          setState(() {
+            mutlipleLang += "${lang.languageTag}, avec une confiance de : ${lang.confidence *100}%";
+          });
+
+        }
+      }
+
+  }
+
+
+  @override
+  void initState() {
+    _languageIdentifier = LanguageIdentifier(confidenceThreshold: 0.3);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _languageIdentifier.close();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+
     return Scaffold(
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
@@ -85,37 +121,37 @@ class _MyHomePageState extends State<MyHomePage> {
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
+
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            TextField(
+              controller: controller,
+
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+
+
+            Text("une langue identifié : $simpleLang"),
+
+            Text("les langues identifiées  : $mutlipleLang"),
+
+            ElevatedButton(
+                onPressed: getUniqueLanguage,
+
+                child: const Text("Idenfier la langue")
             ),
+
+            ElevatedButton(
+                onPressed: getMultipleLanguage,
+
+                child: const Text("Idenfier la langue")
+            ),
+
+
+
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+
     );
   }
 }
